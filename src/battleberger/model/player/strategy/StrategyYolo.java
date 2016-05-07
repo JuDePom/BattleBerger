40415a631b11662be755adceceb69efca99f389f
lesquoyb <lesquoy.baptiste@gmail.com>
2016-05-07 15:11:15 +0200

    orientation aléatoire pour la strat aléatoire

--- src/battleberger/model/player/strategy/StrategyYolo.java
+++ src/battleberger/model/player/strategy/StrategyYolo.java
@@ -35,7 +35,7 @@ public class StrategyYolo implements IStrategy {
 		while(disp > 0 ){
 			AbstractShip ship = AbstractShipyard.orderShip(s[r.nextInt(s.length)]);
 			Orientation orient = Orientation.values()[r.nextInt(Orientation.values().length)];
-			ship.setOrient(Orientation.South);
+			ship.setOrient(orient);
 			if(disp - ship.shipValue() >= 0){	
 				
 				boolean[][] shape = ship.getShape();
