2cca5a851bd02b7b6d24a082d1be7c031a4e873f
Olinf <hormainloic@hotmail.fr>
2016-05-22 01:47:52 +0200

    panel era un peu plus beau (un chouya)

--- src/battleberger/view/EraPanel.java
+++ src/battleberger/view/EraPanel.java
@@ -1,8 +1,12 @@
 package battleberger.view;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -16,13 +20,36 @@ public class EraPanel extends JPanel{
 	public EraPanel(){
 		super();
 		JButton m_a = new JButton("Moyen age");
-		JButton sheep = new JButton("L'âge des moutons");
-
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		JButton sheep = new JButton("L'age des moutons");
+		sheep.setPreferredSize(new Dimension(200,50));
+		sheep.setFocusable(false);
+		this.setBackground(Color.black);
 		JButton troll = new JButton("Troll ?!");
 		
+		troll.setPreferredSize(new Dimension(200,50));
+		troll.setFocusable(false);
+		JPanel tmp=new JPanel();
+		JPanel tmp2=new JPanel();
+		tmp.setBackground(Color.black);
+		tmp2.setBackground(Color.black);
 		//add(m_a);
-		add(sheep);
-		add(troll);
+		for(int i=0;i<5;i++)
+		{
+			JPanel tmp3=new JPanel();
+			tmp3.setBackground(Color.black);
+			add(tmp3);
+		}
+		this.setBackground(Color.black);
+		tmp.add(sheep);
+		add(tmp);
+		tmp2.add(troll);
+		add(tmp2);
+		for(int i=0;i<5;i++){
+			JPanel tmp3=new JPanel();
+			tmp3.setBackground(Color.black);
+			add(tmp3);
+		}
 		
 		m_a.addActionListener(new ActionListener() {
 			
